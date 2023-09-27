cbf664ddab30d626c013823ff288a3bca9e35124
Moritz Fuller <moritz.fuller@dfinity.org>
2023-09-27 13:00:14 -0300

    🎨 display current principal and dont require webpack changes

--- src/ii_integration_frontend/src/index.js
+++ src/ii_integration_frontend/src/index.js
@@ -1,19 +1,58 @@
-import { ii_integration_backend } from "../../declarations/ii_integration_backend";
+import {
+    createActor,
+    ii_integration_backend,
+} from "../../declarations/ii_integration_backend";
+import { AuthClient } from "@dfinity/auth-client";
+import { HttpAgent, Actor } from "@dfinity/agent";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+let actor = ii_integration_backend;
 
-  const name = document.getElementById("name").value.toString();
+console.log(process.env.CANISTER_ID_INTERNET_IDENTITY);
 
-  button.setAttribute("disabled", true);
+const greetButton = document.getElementById("greet");
+greetButton.onclick = async (e) => {
+    e.preventDefault();
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await ii_integration_backend.greet(name);
+    greetButton.setAttribute("disabled", true);
 
-  button.removeAttribute("disabled");
+    // Interact with backend actor, calling the greet method
+    const greeting = await actor.greet(
+        (await Actor.agentOf(actor).getPrincipal()).toString()
+    );
 
-  document.getElementById("greeting").innerText = greeting;
+    greetButton.removeAttribute("disabled");
 
-  return false;
-});
+    document.getElementById("greeting").innerText = greeting;
+
+    return false;
+};
+
+const loginButton = document.getElementById("login");
+loginButton.onclick = async (e) => {
+    e.preventDefault();
+
+    // create an auth client
+    let authClient = await AuthClient.create();
+
+    // start the login process and wait for it to finish
+    await new Promise((resolve) => {
+        authClient.login({
+            identityProvider:
+                process.env.DFX_NETWORK === "ic"
+                    ? "https://identity.ic0.app"
+                    : `http://localhost:4943/?canisterId=rdmx6-jaaaa-aaaaa-aaadq-cai`,
+            onSuccess: resolve,
+        });
+    });
+
+    // At this point we're authenticated, and we can get the identity from the auth client:
+    const identity = authClient.getIdentity();
+    // Using the identity obtained from the auth client, we can create an agent to interact with the IC.
+    const agent = new HttpAgent({ identity });
+    // Using the interface description of our webapp, we create an actor that we use to call the service methods.
+    actor = createActor(process.env.CANISTER_ID_II_INTEGRATION_BACKEND, {
+        agent,
+    });
+
+    return false;
+};
